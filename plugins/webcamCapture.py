726511c47fa5cfb7530a958e17ff152977e0d0c7
martin882003 <martin882003@gmail.com>
2013-11-07 21:32:25 -0300

    Webcam capture

---
+++ plugins/webcamCapture.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+# webcamCapture.py
+ 
+from pygame.image import save
+import pygame.camera as camera
+from os import environ, mkdir, listdir
+from time import strftime, gmtime
+ 
+class WebcamCapture(object):
+        def __init__(self):
+ 
+                camera.init()
+                misWebcams = camera.list_cameras()
+ 
+                if len(misWebcams) == 0:
+                        raise Exception('No hay webcam disponible.')
+                        exit()
+ 
+                elif len(misWebcams) == 1:
+                        self.miWebcam = misWebcams[0]
+ 
+                else:
+                        for i in range(len(misWebcams)):
+                                try:
+                                        self.miWebcam = misWebcams[i]
+                                        break
+                                except:
+                                        continue
+ 
+        def capturar(self):
+                try:
+                        webcam = camera.Camera(self.miWebcam,(640,480))
+                        webcam.start()
+ 
+                        self.captura = webcam.get_image()
+                        
+                        webcam.stop()
+                except Exception as e:
+                        print e
+ 
+        def guardarCaptura(self):
+                self.usuario = environ['USER']
+ 
+                if not 'webcam' in listdir('./'):
+                        mkdir('webcam')
+ 
+                tiempo = strftime("%d %b %Y_%H:%M:%S", gmtime())
+                imagen = './webcam/' + self.usuario + '_' + tiempo + '.png'
+ 
+                save(self.captura, imagen)
+ 
+def main():
+        wcCapture = WebcamCapture()
+        wcCapture.capturar()
+        wcCapture.guardarCaptura()
+ 
+if __name__ == '__main__':
+        main()
